c8c9f7520861e0d3f5282c98b38d210e8b5b1e03
AbdulWahabRaza123 <abdulwahabraza123@gmail.com>
2023-12-24 21:18:44 +0500

    added vercel envs

--- config/plugin.js
+++ config/plugin.js
@@ -1,5 +1,12 @@
 module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
+    config: {
+      deployHook: process.env.DEPLOY_HOOK,
+      apiToken: process.env.API_TOKEN,
+      appFilter: process.env.APP_FILTER,
+      teamFilter: "none",
+      roles: ["strapi-super-admin"],
+    },
   },
 });
